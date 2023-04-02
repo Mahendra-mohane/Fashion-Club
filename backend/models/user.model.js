b83a1bf80d465e18cf71baa7e663df3252eb96da
Mahendra-mohane <itsmahendramohane11@gmail.com>
2023-04-02 15:46:07 +0530

    updates

---
+++ backend/models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const userSchema  = mongoose.Schema({
+name:{type:String},
+email:{type:String},
+phone:{type:String},
+password:{type:String},
+})
+
+const UserModel = mongoose.model("user",userSchema)
+
+module.exports = {
+    UserModel
+}
